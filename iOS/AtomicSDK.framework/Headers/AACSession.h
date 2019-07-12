a65248715435f4b47ef94aa3a5331ce83c1c1bb8
smctainsh <42599390+smctainsh@users.noreply.github.com>
2019-07-12 16:07:34 +1200

    Release 0.2.0 (#5)
    Assets for release 0.2.0 of the iOS SDK.

--- iOS/AtomicSDK.framework/Headers/AACSession.h
+++ iOS/AtomicSDK.framework/Headers/AACSession.h
@@ -5,6 +5,28 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "AACSessionDelegate.h"
+
+/**
+ Completion handler called when a request for the card count in a stream container is fulfilled.
+ 
+ @param cardCount The count of the cards in the stream container, or `nil` if the request failed.
+ */
+typedef void(^AACSessionCardCountCompletionHandler)(NSNumber * __nullable cardCount);
+
+/**
+ Notification posted when the number of cards in a stream container changes.
+ 
+ - The notification's `object` corresponds to the stream container ID, as an NSString.
+ - Use the `AACSessionCardCountUserInfoKey` key to retrieve the card count from the notification's `userInfo` dictionary.
+ */
+extern NSString* __nonnull const AACSessionCardCountDidChange;
+
+/**
+ The key for an NSNumber object containing the number of cards now in the stream container
+ with the given ID.
+ */
+extern NSString* __nonnull const AACSessionCardCountUserInfoKey;
 
 /**
  A singleton that spans the SDK's lifecycle, and oversees all instances of stream
@@ -15,7 +37,7 @@
  */
 @interface AACSession: NSObject
 
-- (instancetype)init NS_UNAVAILABLE;
+- (instancetype __nonnull)init NS_UNAVAILABLE;
 
 /**
  Sets whether debug logging should be enabled within the SDK. This can be useful in debug
@@ -30,5 +52,18 @@
  or the active user changes.
  */
 + (void)logout;
+
+/**
+ Asks the SDK for a count of all cards in the stream container with the given ID.
+ 
+ @param streamContainerId (Required) The stream container ID to return the card count for.
+ @param sessionDelegate (Required) A delegate that supplies a user authentication token when requested
+ by the SDK.
+ @param completionHandler (Required) A completion handler called when the request for the card
+ count is fulfilled.
+ */
++ (void)requestCardCountForStreamContainerWithId:(NSString* __nonnull)streamContainerId
+                                 sessionDelegate:(id<AACSessionDelegate> __nonnull)sessionDelegate
+                               completionHandler:(AACSessionCardCountCompletionHandler __nonnull)completionHandler;
 
 @end
