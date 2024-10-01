0f1f31016785d670bd48515c66d1b7ef4ebc35b6
Atomic Releases <ci@atomic.io>
2024-10-01 00:37:07 +0000

    Release 23.4.10

--- iOS/AtomicSDK.xcframework/ios-arm64_x86_64-simulator/AtomicSDK.framework/Headers/AACCardNodeMedia.h
+++ iOS/AtomicSDK.xcframework/ios-arm64_x86_64-simulator/AtomicSDK.framework/Headers/AACCardNodeMedia.h
@@ -6,8 +6,6 @@
 
 #import <AtomicSDK/AACCardNode.h>
 
-extern CGFloat const AACCardNodeMediaHeightDynamic;
-
 /**
  Media element kinds (or types) supported by the SDK.
  */
@@ -32,34 +30,6 @@ typedef NS_ENUM(NSUInteger, AACCardNodeMediaFormat) {
     AACCardNodeMediaFormatText,
     /// The media should be rendered as a small thumbnail with metadata.
     AACCardNodeMediaFormatThumbnail
-};
-
-/**
- Possible actions that a media element can take.
- */
-typedef NS_ENUM(NSUInteger, AACCardNodeMediaActionType) {
-    /// The media would display an image or video.
-    AACCardNodeMediaActionTypeMedia,
-    /// The media would redirect to a URL.
-    AACCardNodeMediaActionTypeURL,
-    /// The media would redirect to a subview.
-    AACCardNodeMediaActionTypeSubview,
-    /// The media would redirect to a custom payload.
-    AACCardNodeMediaActionTypePayload
-};
-
-/**
- Defines the possible heights for displaying a media element.
- */
-typedef NS_ENUM(NSUInteger, AACCardNodeMediaHeight) {
-    /// Tall height (200px). Spans full width, centered with automatic vertical cropping.
-    AACCardNodeMediaHeightTall,
-    /// Medium height (120px). Spans full width, centered with automatic vertical cropping.
-    AACCardNodeMediaHeightMedium,
-    /// Short height (50px). Spans full width, centered with automatic vertical cropping.
-    AACCardNodeMediaHeightShort,
-    /// Original size. Displays the source image at its original dimensions, spanning full width with no cropping.
-    AACCardNodeMediaHeightOriginal
 };
 
 @interface AACCardNodeMedia: AACCardNode
@@ -87,29 +57,5 @@ typedef NS_ENUM(NSUInteger, AACCardNodeMediaHeight) {
 
 /// The alternate text, provided in the Atomic Workbench, that describes the source media.
 @property (nonatomic, copy, nullable) NSString *alternateText;
-
-/// The action URL to jump to when set in the Atomic Workbench.
-@property (nonatomic, strong, nullable) NSURL* actionUrl;
-
-/// The name of the layout to navigate to on tap. If this property is `nil`, the document at `url` will be presented instead.
-@property (nonatomic, copy, nullable) NSString* actionLayoutName;
-
-/**
- A payload of key-value pairs that are sent to the host app, to determine the custom action taken
- when tapping on this button. We use the same callback in the `AACStreamContainerActionDelegate` that's used by a link button.
- */
-@property (nonatomic, copy, nullable) NSDictionary *actionPayload;
-
-/**
- The action to be taken by this object.
- */
-@property (nonatomic) AACCardNodeMediaActionType actionType;
-
-/**
- The specified height type of this object provided in the Atomic Workbench.
- 
- Defaults to `AACCardNodeMediaHeightTall`.
- */
-@property (nonatomic) AACCardNodeMediaHeight displayedHeightType;
 
 @end
